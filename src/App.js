f471c55ae5a580df8d6af7565fe7c977ffc17373
Aakash Sharma <sharmaaakash824@gmail.com>
2022-05-12 17:45:24 +0530

    Mazor update - fetch data from api and show it

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Row from './Components/Row';
+import Banner from './Components/Banner';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-     
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} isLargeRow={true} />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
